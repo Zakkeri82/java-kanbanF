5d605f3f4090362304b09948ab92e6db1be8a696
Vera_Z <zhenih-v@mail.ru>
2024-03-04 15:55:19 +0300

    ТЗ 7 вторая версия_добавила в классе Managers пустую строку перед методом  getDefault()

--- src/managers/Managers.java
+++ src/managers/Managers.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 public class Managers {
     public static final File FILE = new File("tasks.csv");
+
     public static TaskManager getDefault() {
         return new FileBackedTaskManager(FILE);
     }
